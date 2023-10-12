973c11217fd8a8cad6768f171d4a19f25f76cbe1
PoushnayaA <poushnaya@bk.ru>
2023-10-13 02:36:05 +0300

    Add navigation-block + PixelPerfect

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,17 @@
 /* в этот файл добавляет скрипты*/
+const toggleButton = document.querySelector('.navigation__toggle')
+const navigation = document.querySelector('.navigation')
+const navigationList = document.querySelector('.navigation__list')
+
+
+toggleButton.addEventListener('click', function () {
+  if (navigation.classList.contains('navigation--closed')) {
+    navigation.classList.remove('navigation--closed');
+    navigation.classList.add('navigation--opened');
+    navigationList.classList.remove('navigation__list--closed')
+  } else {
+    navigation.classList.add('navigation--closed');
+    navigation.classList.remove('navigation--opened');
+    navigationList.classList.add('navigation__list--closed')
+  }
+});
